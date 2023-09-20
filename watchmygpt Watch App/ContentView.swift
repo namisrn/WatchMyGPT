14268cb1f31adf7aa5737792fe6aa026a66724f0
Sasan Rafat Nami <Nami.srn@gmail.com>
2023-09-20 15:40:08 +0200

    Initial Commit

---
+++ watchmygpt Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  watchmygpt Watch App
+//
+//  Created by Sasan Rafat Nami on 20.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ watchmygpt Watch App/watchmygptApp.swift
@@ -0,0 +1,17 @@
+//
+//  watchmygptApp.swift
+//  watchmygpt Watch App
+//
+//  Created by Sasan Rafat Nami on 20.09.23.
+//
+
+import SwiftUI
+
+@main
+struct watchmygpt_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
