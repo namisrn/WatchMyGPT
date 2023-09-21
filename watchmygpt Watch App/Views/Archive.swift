495ad54cef4908af457febd1eacf5b7af3222e93
Sasan Rafat Nami <Nami.srn@gmail.com>
2023-09-21 06:03:15 +0200

    Mwnu + Archive and Setting placeholder

---
+++ watchmygpt Watch App/Views/Archive.swift
@@ -0,0 +1,18 @@
+//
+//  Archive.swift
+//  watchmygpt Watch App
+//
+//  Created by Sasan Rafat Nami on 21.09.23.
+//
+
+import SwiftUI
+
+struct Archive: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Archive()
+}
